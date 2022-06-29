6373b93670cb2195320e98ca74fc444fdaeca360
Devis <tumuhaise@gmail.com>
2022-06-29 16:51:27 -0700

    4.Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,2 +1,8 @@
 #include <stdio.h>
-puts("\"Programming is like building a multilingual puzzle");
+
+int main()
+{
+	char str[]="\"Programming is like building a multilingual puzzle";
+	puts(str);
+	return 0;
+}
