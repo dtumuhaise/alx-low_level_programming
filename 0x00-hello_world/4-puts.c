4811509da8c6ca931d71d255b0ea9a27402aaf99
Devis <tumuhaise@gmail.com>
2022-06-29 17:25:01 -0700

    updated

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,13 +2,13 @@
 /**
  *  main(void) - main function will be called without any parameter
  *  print out value of str using puts()
+ *  Return: 0 at the end of the program
  */  
 int main(void)
 {
-	/* initalize variable*/
 	char str[] = "\"Programming is like building a multilingual puzzle";
 
-	puts(str); /* write string to stdout */
+	puts(str);
 
 	return (0);
 }
