a6cc65eb09ca984881992b9bc26bb53dc289e5b2
Devis <tumuhaise@gmail.com>
2022-06-30 15:19:00 -0700

    updated

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -2,12 +2,18 @@
 /** main - runs without parameters
  * Return: Returns 0 at the end of the program
  */
-int main ()
+int main()
 {
-	printf("Size of a char: %d\n" sizeof(char));
-	printf("Size of an int: %d\n" sizeof(int));
-	printf("Size of a long int: %d\n" sizeof(long int));
-	printf("Size of a long long int: %d\n" sizeof(long long int));
-	printf("Size of a float: %d\n" sizeof(float));
+	char a;
+	int b;
+	long int c;
+	long long int d;
+	float e;
+
+	printf("Size of a char: %d\n" sizeof(a));
+	printf("Size of an int: %d\n" sizeof(b));
+	printf("Size of a long int: %d\n" sizeof(c));
+	printf("Size of a long long int: %d\n" sizeof(d));
+	printf("Size of a float: %d\n" sizeof(e));
 	return (0);
 }
