63ebf3da4302ab912d715e566a4d5da3cab5d490
Devis <tumuhaise@gmail.com>
2022-06-29 17:00:01 -0700

    updated

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-int main(void)
+int main(void) /* program to print a string of characters using print() */
 {
 	char str[] = "\"Programming is like building a multilingual puzzle";
 
