0659311c850ffb6120d3f7ebec4a41a89b15e241
Devis <tumuhaise@gmail.com>
2022-06-30 13:32:08 -0700

    5. Hello, printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+/**
+ * main - This is a function
+ * return: 0
+ */
+int main(void)
+{
+	printf("%s \n", "with proper grammar\, but the outcome is a piece of art\,");
+	return (0);
+}
