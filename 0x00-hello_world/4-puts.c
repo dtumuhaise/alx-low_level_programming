c54710957c50bf9dad63cecdb08609a49ced52d8
Devis <tumuhaise@gmail.com>
2022-06-30 13:14:49 -0700

    Updated

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,7 +3,7 @@
  *  main - main function will be called without any parameter
  *  print out value of str using puts()
  *  Return: 0 at the end of the program
- */  
+ */
 int main(void)
 {
 	char str[] = "\"Programming is like building a multilingual puzzle";
