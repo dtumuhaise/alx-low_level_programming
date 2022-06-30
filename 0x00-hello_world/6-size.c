78a3d4194c22436ef32470ce8773cc4b6507078a
Devis <tumuhaise@gmail.com>
2022-06-30 15:51:25 -0700

    updated

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -2,7 +2,7 @@
 /** main - runs without parameters
  * Return: Returns 0 at the end of the program
  */
-int main()
+int main(void)
 {
 	char a;
 	int b;
@@ -10,10 +10,10 @@ int main()
 	long long int d;
 	float e;
 
-	printf("Size of a char: %d\n", sizeof(a));
-	printf("Size of an int: %d\n", sizeof(b));
-	printf("Size of a long int: %d\n", sizeof(c));
-	printf("Size of a long long int: %d\n", sizeof(d));
-	printf("Size of a float: %d\n", sizeof(e));
+	printf("Size of a char: %d\n bytes(s)", sizeof(a));
+	printf("Size of an int: %d\n bytes(s)", sizeof(b));
+	printf("Size of a long int: %d\n byte(s)", sizeof(c));
+	printf("Size of a long long int: %d\n byte(s)", sizeof(d));
+	printf("Size of a float: %d\n bytes(s)", sizeof(e));
 	return (0);
 }
