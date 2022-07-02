51263320168eccef76a68015ff8ec53d9a63a7d9
Devis <tumuhaise@gmail.com>
2022-07-02 05:20:01 -0700

    Numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - function takes no arguments
+ * Return: returns 0 after execution
+ */
+int main(void)
+{
+	int i;
+
+	for (i=0; i < 10; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
