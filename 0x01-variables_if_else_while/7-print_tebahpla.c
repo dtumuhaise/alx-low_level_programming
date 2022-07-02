58ddb8dabc7fa69d16f9dfdddf7462778f1173ba
Devis <tumuhaise@gmail.com>
2022-07-02 12:47:44 -0700

    7. Smile in the mirror

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Void function takes no parameters
+ * Return: Returns 0 after executing
+ */
+int main(void)
+{
+	char ch;
+
+	for (ch = 'z'; ch >= 'z' ch--)
+	{
+		putchar(ch);
+	}
+	putchar('\n');
+	return (0);
+}
