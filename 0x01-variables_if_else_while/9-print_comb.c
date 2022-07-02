213fc522f20d5ecb3aff71991276aa2afdfb19a8
Devis <tumuhaise@gmail.com>
2022-07-02 13:34:58 -0700

    update

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -7,12 +7,12 @@ int main(void)
 {
 	int i;
 
-	for (i = 0; i <= 9; i++)
+	for (i = 0; i < 9; i++)
 	{
 		putchar(i + '0');
 		putchar(',');
 		putchar(' ');
 	}
-	putchar('\n');
+	putchar(9 + '0');
 	return (0);
 }
