6c56fcbbe1ee487a4fd6b22d96202c165716b2ec
Devis <tumuhaise@gmail.com>
2022-07-05 07:44:57 -0700

    update

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -10,11 +10,11 @@ int print_sign(int n)
 	if (n > 0)
 	{
 		_putchar(43);
-		return (0);
+		return (1);
 		if (n == 0)
 		{
 			_putchar(0);
-			return(0);
+			return (0);
 			if (n < 0)
 			{
 				_putchar(45);
