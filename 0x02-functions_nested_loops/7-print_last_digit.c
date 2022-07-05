6a13dfdfa0444a5b37e0d4f513268810b324dd11
Devis <tumuhaise@gmail.com>
2022-07-05 14:01:16 -0700

    Last digit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -8,6 +8,14 @@ int print_last_digit(int n)
 {
 	int lastdigit;
 
-	lastdigit = n % 10;
+	if (n > 0)
+	{
+		lastdigit = (n % 10) * -1;
+	}
+	else
+	{
+		lastdigit = n % 10;
+	}
+	_putchar(lastdigit + '0');
 	return (lastdigit);
 }
