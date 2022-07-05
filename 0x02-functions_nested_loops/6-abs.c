21bf3a6837fbb05e2d75015c94cc01cfb88c414c
Devis <tumuhaise@gmail.com>
2022-07-05 08:17:04 -0700

    ^. Asolute value of integer

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * -abs - comptues absolute integer
+ *
+ *  Return: 0
+ */
+int _abs(int)
+{
+	int n;
+
+	if ((n <= 0) || (n >= 0))
+	{
+		_putchar(n + '0');
+	}
+	return (0);
+}
