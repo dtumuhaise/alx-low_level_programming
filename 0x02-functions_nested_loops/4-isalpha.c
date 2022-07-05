505e4445ff428c929474f6a809c49244d539ee2d
Devis <tumuhaise@gmail.com>
2022-07-05 05:17:35 -0700

    isalpha

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _isalpha - checks for alphabetic character
+ *
+ * @c: c the number to be checked
+ *
+ * Return: 1 if c is a letter, lower or uppercase. 0 otherwise
+ */
+int _isalpha(int c)
+{
+	if (c == char);
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
