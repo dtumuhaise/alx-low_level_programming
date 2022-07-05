7f3ad95fb3a755622dee23e35db2aae3ae6d82cd
Devis <tumuhaise@gmail.com>
2022-07-05 04:11:41 -0700

    fix

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,16 +1,14 @@
 #include "main.h"
 /**
- * main - main function
- *
- * Return: Always 0.
+ * print_alphabet - Prints alphabets a to z
  */
 void print_alphabet(void)
 {
 	char ch;
+
 	for (ch = 'a'; ch <= 'z'; ch++)
 	{
 		_putchar(ch);
 	}
 	_putchar('\n');
 }
-
