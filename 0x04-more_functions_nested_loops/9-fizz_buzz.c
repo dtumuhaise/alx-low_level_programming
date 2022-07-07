bab52374e81fe225d9b71066eb00db3320c55a57
Devis <tumuhaise@gmail.com>
2022-07-07 13:51:45 -0700

    fix

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -10,7 +10,11 @@ int main(void)
 
 	for (i = 1; i <= 100; i++)
 	{
-		if (i % 3 == 0)
+		if ((i % 3 == 0) && (i % 5 == 0))
+		{
+			printf("FizzBuzz");
+		}
+		else if (i % 3 == 0)
 		{
 
 			printf("Fizz");
@@ -18,10 +22,6 @@ int main(void)
 		else if (i % 5 == 0)
 		{
 			printf("Buzz");
-		}
-		else if ((i % 3 == 0) && (i % 5 == 0))
-		{
-			printf("FizzBuzz");
 		}
 		else
 		{
