482facd949bee5614415b95f048df76415843815
Devis <tumuhaise@gmail.com>
2022-07-11 14:29:58 -0700

    6. Half the lies they tell about me aren't true

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * puts2 - prints ever other character
+ * @str: pointer
+ */
+void puts2(char *str)
+{
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		if (i % 2 == 0)
+		{
+			_putchar(str[i]);
+		}
+		i++;
+	}
+	_putchar('\n');
+}
