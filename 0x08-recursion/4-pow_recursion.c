e6ec231a0c5b8a9e20c58121ca74e6727b352cfa
Devis <tumuhaise@gmail.com>
2022-07-20 04:15:29 -0700

    fix

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -9,9 +9,12 @@
 
 int _pow_recursion(int x, int y)
 {
-	if (y < 0)
+	if (y > 0)
+	{
+		return (x * _pow_recursion(x, y - 1));
+	}
+	else
 	{
 		return (-1);
 	}
-	return (x * _pow_recursion(x, y - 1));
 }
