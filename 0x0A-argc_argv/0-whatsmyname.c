a7496c594117c313a7e3505c34890609a5ebc833
Devis <tumuhaise@gmail.com>
2022-07-22 08:45:30 -0700

    0. It ain't what they call you, it's what you answer to

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * main - function that prints its name
+ * @argc: number of arguments
+ * @argv: array
+ * Return: int
+ */
+
+int main(int argc, char *argv[])
+{
+	int i;
+
+	for (i = 0; i < argc; i++)
+	{
+		printf("%s ", argv[i]);
+	}
+	printf("\n");
+	return (0);
+}
