5e08a5e1e177f66a6faeba5f70657d115b8acdc0
Devis <tumuhaise@gmail.com>
2022-08-03 05:51:25 -0700

    fix

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,5 +1,4 @@
 #include "function_pointers.h"
-#include <stdio.h>
 /**
  * print_name - prints a name
  * @name: the name
@@ -12,4 +11,5 @@ void print_name(char *name, void (*f)(char *))
 	{
 		(*f)(name);
 	}
+	_putchar('\n');
 }
--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,16 +1,10 @@
-#ifndef _FUNCTION_POINTERS_H_
-#define _FUNCTION_POINTER_H_
-
-
 #include <stdio.h>
 
+#ifndef FUNCTION_POINTERS_H
+#define FUNCTION_POINTERS_H
+
 void print_name(char *name, void (*f)(char *));
-
 void array_iterator(int *array, size_t size, void (*action)(int));
-
 int int_index(int *array, int size, int (*cmp)(int));
-
 int _putchar(char c);
-
-
 #endif
