d6392d175ccc251eb103975ddf45ee16068672bc
Devis <tumuhaise@gmail.com>
2022-08-03 05:17:35 -0700

    Whats my name

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,15 @@
+#include "function_pointers.h"
+
+/**
+ * print_name - prints a name
+ * @name: the name
+ * @f: function pointer
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	if (f)
+	{
+		(*f)(name);
+	}
+}
--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -10,5 +10,7 @@ void array_iterator(int *array, size_t size, void (*action)(int));
 
 int int_index(int *array, int size, int (*cmp)(int));
 
+int _putchar(char c);
+
 
 #endif
