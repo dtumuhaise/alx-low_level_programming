a976b3df8775691a86b6e334acf459df71470b71
Devis <tumuhaise@gmail.com>
2022-08-03 12:15:11 -0700

    update

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -12,8 +12,11 @@ void array_iterator(int *array, int size, void (*action)(int))
 {
 	int i;
 
-	for (i = 0; i < size; i++)
+	if (array && action)
 	{
-		action(array[i]);
+		for (i = 0; i < size; i++)
+		{
+			action(array[i]);
+		}
 	}
 }
