fd79ed8b3459dfc7ca4ef8bc79b69d748d420674
Devis <tumuhaise@gmail.com>
2022-08-16 10:19:23 -0700

    fix

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -19,6 +19,10 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 	{
 		head = head->next;
 		count++;
+		if (head == NULL)
+		{
+			return (NULL);
+		}
 	}
 	return (head);
 }
