d0da87fd4ebecc5b0a5b2e2d0cc1adf239e93dc1
Devis <tumuhaise@gmail.com>
2022-08-23 09:07:01 -0700

    Create file

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+ * create_file - creates a file
+ * @filename: name of file to create
+ * @text_content: contents of file
+ * Return: 1 0r -1
+ */
+
+int create_file(const char *filename, char *text_content)
+{
+	int fd, w, len;
+
+	if (filename == NULL)
+	{
+		return (-1);
+	}
+	fd = open(filename, O_CREAT, S_IRUSR | S_IXUSR | O_TRUNC);
+	if (fd == -1)
+	{
+		return (-1);
+	}
+	if (text_content != NULL)
+	{
+		while (text_content[len])
+		{
+			len++;
+		}
+		w = write(fd, text_content, len);
+		if (w != len)
+		{
+			return (-1);
+		}
+	}
+	close(fd);
+	return (1);
+}
