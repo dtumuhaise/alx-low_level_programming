fff8b5b056a2ea42d82bbaed9f1420f55b979063
Devis <tumuhaise@gmail.com>
2022-08-23 09:13:13 -0700

    fix

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -15,7 +15,7 @@ int create_file(const char *filename, char *text_content)
 	{
 		return (-1);
 	}
-	fd = open(filename, O_CREAT, S_IRUSR | S_IXUSR | O_TRUNC);
+	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC);
 	if (fd == -1)
 	{
 		return (-1);
