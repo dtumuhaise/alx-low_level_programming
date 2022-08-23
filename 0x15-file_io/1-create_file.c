2c53c46d404f712608568d11312c0444401cef7e
Devis <tumuhaise@gmail.com>
2022-08-23 09:17:02 -0700

    fix

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -9,13 +9,14 @@
 
 int create_file(const char *filename, char *text_content)
 {
-	int fd, w, len;
+	int fd, w;
+	int len = 0;
 
 	if (filename == NULL)
 	{
 		return (-1);
 	}
-	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC);
 	if (fd == -1)
 	{
 		return (-1);
